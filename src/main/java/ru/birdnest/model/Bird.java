fb7ce4ac8a69092c0aad77a720d5fe822b069903
Stepan Pozolotin <pso@irbis.plus>
2021-08-15 12:14:31 +0400

    BN-1 add flayWay

--- src/main/java/ru/birdnest/model/Bird.java
+++ src/main/java/ru/birdnest/model/Bird.java
@@ -1,10 +1,30 @@
 package ru.birdnest.model;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table()
 public class Bird {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
     private String name;
+    @Column(name = "coolor")
     private String coolor;
+    @Column(name = "fly")
     private boolean fly;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
